0a75c8653648340afbb1d91aea8cdb632b628c24
William da Silva Bezerra <williamsilva754@gmail.com>
2021-06-29 16:08:41 -0300

    add app test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -9,7 +9,36 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets(
-    "should buy a product without problems",
+    "should throw an error when try buy a offer",
+    (tester) async {
+      await tester.pumpWidget(AppWidget());
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(HomePage), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byKey(Key("offers")), findsOneWidget);
+
+      await tester.tap(find.byKey(Key("offer2")));
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byKey(Key("purchase")));
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(SnackBar), findsOneWidget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(PurchasePage), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "should buy an offer when try with correct informations",
     (tester) async {
       await tester.pumpWidget(AppWidget());
 
@@ -38,22 +67,6 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(find.byType(HomePage), findsOneWidget);
-
-      expect(find.text("\$0"), findsOneWidget);
-
-      await tester.tap(find.byKey(Key("offer2")));
-
-      await tester.pumpAndSettle();
-
-      await tester.tap(find.byKey(Key("purchase")));
-
-      await tester.pumpAndSettle();
-
-      expect(find.byType(SnackBar), findsOneWidget);
-
-      await tester.pumpAndSettle();
-
-      expect(find.byType(PurchasePage), findsOneWidget);
     },
   );
 }
