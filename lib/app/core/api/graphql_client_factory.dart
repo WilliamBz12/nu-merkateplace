3dbd2bd8959f05b25abaea719c09606137613def
William da Silva Bezerra <williamsilva754@gmail.com>
2021-06-29 19:07:09 -0300

    add .env

--- lib/app/core/api/graphql_client_factory.dart
+++ lib/app/core/api/graphql_client_factory.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class ApiClientFactory {
@@ -5,8 +6,7 @@ class ApiClientFactory {
     final httpLink = HttpLink(
       "https://staging-nu-needful-things.nubank.com.br/query",
       defaultHeaders: <String, String>{
-        'Authorization':
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhd2Vzb21lY3VzdG9tZXJAZ21haWwuY29tIn0.cGT2KqtmT8KNIJhyww3T8fAzUsCD5_vxuHl5WbXtp8c',
+        'Authorization': 'Bearer ${dotenv.env["BASE_TOKEN"]}',
       },
     );
     return GraphQLClient(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app/app_widget.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: "assets/.env");
   runApp(AppWidget());
 }
